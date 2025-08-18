b203e6e730e1ffdcf719c7a09e1a53190be33754
Muhammed Taha <zedanmhmd41@gmail.com>
2025-08-19 01:29:17 +0300

    Mocking JavaMailSender for tests

--- backend/src/test/java/com/example/backend/LoginAppApplicationTests.java
+++ backend/src/test/java/com/example/backend/LoginAppApplicationTests.java
@@ -3,16 +3,25 @@ package com.example.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
 
-@SpringBootTest
-@TestPropertySource(properties = {
+@SpringBootTest(properties = {
     "app.upload.dir=uploads",
-    "app.base.url=http://localhost:8080"
+    "app.base.url=http://localhost:8080",
+    "spring.data.mongodb.uri=mongodb://test-uri",
+    "spring.mail.host=smtp.gmail.com",
+    "spring.mail.port=587",
+    "spring.mail.username=zedanmhmd41@gmail.com",
+    "spring.mail.password=felz toxt qesq qyiz"
 })
 class LoginAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockBean
+    private JavaMailSender javaMailSender;
+
+    @Test
+    void contextLoads() {
+    }
 
 }
