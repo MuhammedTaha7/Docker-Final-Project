f593df7abf9cfb07c4225a2a00fbc9665f813fb3
Muhammed Taha <zedanmhmd41@gmail.com>
2025-08-19 01:35:09 +0300

    Fix MongoDB database name in test properties

--- backend/src/test/java/com/example/backend/LoginAppApplicationTests.java
+++ backend/src/test/java/com/example/backend/LoginAppApplicationTests.java
@@ -9,19 +9,17 @@ import org.springframework.mail.javamail.JavaMailSender;
 @SpringBootTest(properties = {
     "app.upload.dir=uploads",
     "app.base.url=http://localhost:8080",
-    "spring.data.mongodb.uri=mongodb://test-uri",
+    "spring.data.mongodb.uri=mongodb://test-uri/testdb", 
     "spring.mail.host=smtp.gmail.com",
     "spring.mail.port=587",
     "spring.mail.username=zedanmhmd41@gmail.com",
     "spring.mail.password=felz toxt qesq qyiz"
 })
 class LoginAppApplicationTests {
-
     @MockBean
     private JavaMailSender javaMailSender;
 
     @Test
     void contextLoads() {
     }
-
 }
